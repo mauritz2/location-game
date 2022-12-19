d7ba10f2514a5a60630de70e67035b4310cce3b7
Giacomo Rebonato <grebonato@outlook.ie>
2022-12-19 23:12:03 +0100

    reorganised code

---
+++ src/types/index.ts
@@ -0,0 +1,19 @@
+export type ResourceObjectAmts = {
+  coins: number;
+  armor: number;
+  herbs: number;
+  scrolls: number;
+  corpses: number;
+};
+
+export type ActionObject = {
+  action: string;
+  location: string;
+  action_details: string | null;
+  player_id?: string;
+};
+
+export type CurrentPlayer = {
+  player_name: string;
+  player_id: string;
+};
